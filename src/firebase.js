3c56e397feef15f6f51eccf854471270d8ac0168
yeonghoon123 <haa3163314@naver.com>
2022-07-27 22:22:24 +0900

    setting

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,15 @@
 import { initializeApp } from "firebase/app";
 
+console.log(process.env.FIREBASE_API_KEY);
+
 // firebase config 정보
 const firebaseConfig = {
-  apiKey: "AIzaSyDDCu8PaADN3cqw0YqoSxuvGyayLFwGTSc",
-  authDomain: "twitterclone-281c7.firebaseapp.com",
-  projectId: "twitterclone-281c7",
-  storageBucket: "twitterclone-281c7.appspot.com",
-  messagingSenderId: "788282970252",
-  appId: "1:788282970252:web:1b30c69b1192d53b0eeba9"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGAGING_SENDERP_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Firebase from './firebase';
+console.log(Firebase);
 
 ReactDOM.render(
   <React.StrictMode>
