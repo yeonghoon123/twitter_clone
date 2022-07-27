df7cdaedcc4d0d3ec4b9a30980af25507e9c0098
yeonghoon123 <haa3163314@naver.com>
2022-07-27 22:26:52 +0900

    firebase key setting

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,19 @@
-import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
 
-console.log(process.env.FIREBASE_API_KEY);
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 
 // firebase config 정보
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGAGING_SENDERP_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGAGING_SENDERP_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
-export default initializeApp(firebaseConfig);
+export default firebase.initializeApp(firebaseConfig);
